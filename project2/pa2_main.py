1d9a0f2074784e445bd14b0f12b7998e9e07f3cd
dlopez83 <43835133+dlopez83@users.noreply.github.com>
2018-10-15 13:11:53 -0600

    Update project2/pa2_main.py

--- project2/pa2_main.py
+++ project2/pa2_main.py
@@ -1,10 +1,12 @@
 if __name__ == '__main__':
 
-	names = ["calvin", "and", "hobbes", "are", "the", "first", "spacemen", "on", "mars"]
+	names = []
 
 	def cap_join(names):
 
 		names2 = " ".join(names).title()
 		return names2
+
+	names = ["calvin", "and", "hobbes", "are", "the", "first", "spacemen", "on", "mars"]
 
 	print(cap_join(names))
