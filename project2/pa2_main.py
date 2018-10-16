762276674bfe14cfd68d5cfe9e4e538145c48fc5
dlopez83 <43835133+dlopez83@users.noreply.github.com>
2018-10-15 19:34:52 -0600

    Update project2/pa2_main.py

--- project2/pa2_main.py
+++ project2/pa2_main.py
@@ -1,14 +1,13 @@
 # Solution 1
 
+# names = []
+
+# def cap_join(names):
+
+# 	names2 = " ".join(names).title()
+# 	return names2
+
 # if __name__ == '__main__':
-
-# 	names = []
-
-# 	def cap_join(names):
-
-# 		names2 = " ".join(names).title()
-# 		return names2
-
 # 	names = ["calvin", "and", "hobbes", "are", "the", "first", "spacemen", "on", "mars"]
 
 # 	print(cap_join(names))
@@ -22,6 +21,7 @@ def cap_join(list1):
         names.append(index1[0].upper() + index1[1:])
     return ' '.join(names)
 
-names2 = ["calvin", "and", "hobbes", "are", "the", "first", "spacemen", "on", "mars"]
-
-print(cap_join(names2))
+if __name__ == '__main__':
+	names2 = ["calvin", "and", "hobbes", "are", "the", "first", "spacemen", "on", "mars"]
+	
+	print(cap_join(names2))
